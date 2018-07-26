2c395e62bb64fc131080310184bc36dad7f63220
Usman <u.ghani2013@gmail.com>
2018-07-26 14:55:44 -0500

    Add process.env.port for heroku deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req,res) => {
     res.send("ShiftBuddy")
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>{
     console.log("Now listening on port: " + PORT);
