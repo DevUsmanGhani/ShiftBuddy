d0309d58ebfaba6e5dded86aa12a3a8a49aa9513
Usman <u.ghani2013@gmail.com>
2018-07-30 11:27:00 -0500

    Incorporate Taimurs routes

--- ShiftBuddy/app.js
+++ ShiftBuddy/app.js
@@ -59,47 +59,7 @@ let managerSchema = new mongoose.Schema({
 let Manager = mongoose.model("Manager", managerSchema);
 
 /*=================================== MANAGER ROUTES ===================================*/
-// GET ALL MANAGERS
-app.get('/managers', function(req,res) {
-	Manager.find({}, function (err, managers) {
-		if (err) return res.status(500).send("The server encountered an internal error. Please retry the request.");
-		res.status(200).send(managers);
-	});
-});
 
-// MAKE A NEW MANAGER
-app.post('/managers', function(req,res) {
-	Manager.create({
-		name: req.body.name,	
-		email : req.body.email
-	}, function (err, manager) {
-		if (err) return res.status(400).send("One of the request inputs is not valid.");
-		res.status(200).send(manager);
-	});
-});
-
-// 
-app.get('/managers/:mid', function(req,res) {
-    Manager.findById(req.params.mid, function (err, manager) {
-        if (err || !manager ) return res.status(404).send("The specified resource does not exist.");
-        res.status(200).send(manager);
-    });
-});
-
-app.put('/managers/:mid', function(req,res) {
-    Manager.findByIdAndUpdate(req.params.mid, req.body, {new: true}, function (err, manager) {
-        if (err || !manager) return res.status(404).send("The specified resource does not exist.");
-        res.status(200).send(manager);
-    });
-});
-
-app.delete('/managers/:mid', function(req,res) {
-    Manager.findByIdAndRemove(req.params.mid, function (err, manager) {
-        if (err || !manager) return res.status(404).send("The specified resource does not exist.");
-        res.status(200).send("Manager '"+ manager.name +"' was deleted.");
-
-    });
-});
 
 // Employee Routes
 app.get('/managers/:mid/employees', function(req,res) {
--- routes/api/managers.js
+++ routes/api/managers.js
@@ -1,5 +1,45 @@
 const router = require('express').Router();
 
-// @@DESC 
-// @@
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
+app.get('/managers', function(req,res) {
+	Manager.find({}, function (err, managers) {
+		if (err) return res.status(500).send("The server encountered an internal error. Please retry the request.");
+		res.status(200).send(managers);
+	});
+});
 
+// MAKE A NEW MANAGER
+app.post('/managers', function(req,res) {
+	Manager.create({
+		name: req.body.name,	
+		email : req.body.email
+	}, function (err, manager) {
+		if (err) return res.status(400).send("One of the request inputs is not valid.");
+		res.status(200).send(manager);
+	});
+});
+
+// 
+app.get('/managers/:mid', function(req,res) {
+    Manager.findById(req.params.mid, function (err, manager) {
+        if (err || !manager ) return res.status(404).send("The specified resource does not exist.");
+        res.status(200).send(manager);
+    });
+});
+
+app.put('/managers/:mid', function(req,res) {
+    Manager.findByIdAndUpdate(req.params.mid, req.body, {new: true}, function (err, manager) {
+        if (err || !manager) return res.status(404).send("The specified resource does not exist.");
+        res.status(200).send(manager);
+    });
+});
+
+app.delete('/managers/:mid', function(req,res) {
+    Manager.findByIdAndRemove(req.params.mid, function (err, manager) {
+        if (err || !manager) return res.status(404).send("The specified resource does not exist.");
+        res.status(200).send("Manager '"+ manager.name +"' was deleted.");
+
+    });
+});
