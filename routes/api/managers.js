aa662c5275fecda3a9c7a8e37c726850d3150256
Usman <u.ghani2013@gmail.com>
2018-07-31 10:40:40 -0500

    Add post for manager

--- routes/api/managers.js
+++ routes/api/managers.js
@@ -1,45 +1,43 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+// Load Manager Model
+const Manager = require('../../models/Manager');
 
-// @route   GET api/users/test
-// @desc    Tests users route
+// @route   GET api/managers/test
+// @desc    Tests Manager route
 // @access  Public
-app.get('/managers', function(req,res) {
-	Manager.find({}, function (err, managers) {
-		if (err) return res.status(500).send("The server encountered an internal error. Please retry the request.");
-		res.status(200).send(managers);
-	});
+router.get('/test', (req, res) => res.json({ msg: "Manager Works!"}));
+
+// @route   POST api/managers
+// @desc    Crate a manager
+// @access  Private
+router.post('/', (req, res) => {
+  //const { errors, isValid } = validateManagerInput(req.body);
+
+  // Check Validation
+ // if(!valid) {
+ //   return res.status(400).json( errors );
+ // }
+
+  Manager.findOne({ email: req.body.email})
+  .then(manager => {
+    if(manager){
+      errors.email = "This email already exists";
+      return res.status(400).json(errors);
+    }
+    else{
+      const newManager = new Manager({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+      newManager
+      .save()
+      .then(newManager => res.json(newManager))
+      .catch(err => res.json(err));
+    }
+  });
 });
 
-// MAKE A NEW MANAGER
-app.post('/managers', function(req,res) {
-	Manager.create({
-		name: req.body.name,	
-		email : req.body.email
-	}, function (err, manager) {
-		if (err) return res.status(400).send("One of the request inputs is not valid.");
-		res.status(200).send(manager);
-	});
-});
-
-// 
-app.get('/managers/:mid', function(req,res) {
-    Manager.findById(req.params.mid, function (err, manager) {
-        if (err || !manager ) return res.status(404).send("The specified resource does not exist.");
-        res.status(200).send(manager);
-    });
-});
-
-app.put('/managers/:mid', function(req,res) {
-    Manager.findByIdAndUpdate(req.params.mid, req.body, {new: true}, function (err, manager) {
-        if (err || !manager) return res.status(404).send("The specified resource does not exist.");
-        res.status(200).send(manager);
-    });
-});
-
-app.delete('/managers/:mid', function(req,res) {
-    Manager.findByIdAndRemove(req.params.mid, function (err, manager) {
-        if (err || !manager) return res.status(404).send("The specified resource does not exist.");
-        res.status(200).send("Manager '"+ manager.name +"' was deleted.");
-
-    });
-});
+module.exports = router;
