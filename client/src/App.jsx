8a7bcbb549e3e03f08f16240134186382c93e76a
Usman <u.ghani2013@gmail.com>
2018-08-01 11:16:30 -0500

    Added bootstrap

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Shift Buddy
+        <hr />
+        <small>Welcome to Shift Buddy!</small>
+      </div>
+    );
+  }
+}
+
+export default App;
