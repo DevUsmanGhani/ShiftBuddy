b434a2a97b5f479ab9ff12b88e938522401a2d30
Usman <u.ghani2013@gmail.com>
2018-08-02 14:15:38 -0500

    Added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Landing from "./components/public/Landing";
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Jumbotron>
-          <h1>
-            Welcome to Shift Buddy!
-          </h1>
-          <span>Revolutionizing the way that your business handles shifts</span>
-          <hr />
-          <Button bsSize="large" bsStyle="info">Register Today</Button>
-        </Jumbotron>
-      </div>
+      <Router>
+        <Route exact path='/'component={Landing} />
+      </Router>
     );
   }
 }
