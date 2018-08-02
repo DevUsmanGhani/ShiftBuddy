a287b4a75d37729f9b5aca309eb03a339795cc6b
TaimurGhani <34312969+TaimurGhani@users.noreply.github.com>
2018-08-02 15:31:58 -0500

    add fontawesome imports to app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,12 @@ import Landing from './components/Public/Landing';
 import SiteNavbar from './components/Common/SiteNavbar';
 import './App.css';
 
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+library.add(faStroopwafel)
+
 
 class App extends Component {
   render() {
