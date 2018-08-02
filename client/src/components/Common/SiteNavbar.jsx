3a04f350ec1bc47dd40e711190826375e2dba73e
TaimurGhani <34312969+TaimurGhani@users.noreply.github.com>
2018-08-02 15:23:32 -0500

    change navbar buttons

--- client/src/components/Common/SiteNavbar.jsx
+++ client/src/components/Common/SiteNavbar.jsx
@@ -4,28 +4,27 @@ import { Nav, Navbar, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 export class SiteNavbar extends Component {
   render() {
     return (
-        <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#home">React-Bootstrap</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
+      <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="/">Shift Buddy</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav pullRight>
           <NavItem eventKey={1} href="#">
-            Link
+            About
           </NavItem>
           <NavItem eventKey={2} href="#">
-            Link
+            Contact
           </NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.4}>Separated link</MenuItem>
-          </NavDropdown>
+          <NavItem eventKey={3} href="#">
+            Login
+          </NavItem>
         </Nav>
-      </Navbar>
+      </Navbar.Collapse>
+    </Navbar>
     )
   }
 }
