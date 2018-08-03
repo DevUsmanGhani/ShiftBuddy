bc269a1d9e00d6e07a1def1dace938e23fd47de0
Usman <u.ghani2013@gmail.com>
2018-08-03 11:39:27 -0500

    Add GET_ERRORS and CLEAR_ERRORS case to errorReducer

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,1 +1,14 @@
 import { GET_ERRORS, CLEAR_ERRORS } from '../actions/types'
+
+initialState = {};
+
+export default (state=initalState, action) => {
+  switch(action.type){
+    case GET_ERRORS: 
+      return action.payload;
+    case CLEAR_ERRORS: 
+      return {};
+    default: 
+      return state
+  }
+}
