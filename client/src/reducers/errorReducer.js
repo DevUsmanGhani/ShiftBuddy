a0f439bc4b2295abb775bbe5bd6aa5707cd6c107
Usman <u.ghani2013@gmail.com>
2018-08-03 11:34:24 -0500

    Add errorReducer and more types

---
+++ client/src/reducers/errorReducer.js
@@ -0,0 +1,1 @@
+import { GET_ERRORS, CLEAR_ERRORS } from '../actions/types'
--- client/src/reducers/manager/managerAuthReducer.js
+++ client/src/reducers/manager/managerAuthReducer.js
@@ -11,7 +11,8 @@ export default (state = initialState, action) => {
     case SET_CURRENT_USER: {
       return {
         ...state,
-        isAuthenticated: !isEmpty(action.payload)
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload,
       }
     }
     default: {
