2598f8983789f52108418598949243c207730f86
Usman <u.ghani2013@gmail.com>
2018-08-03 10:17:47 -0500

    Wrap app.jsx in provide for redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import ManagerLogin from './components/Manager/ManagerLogin.jsx';
 import Landing from './components/Public/Landing';
 import SiteNavbar from './components/Common/SiteNavbar';
@@ -14,16 +15,15 @@ library.add(faSignInAlt)
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <SiteNavbar />
+      <Provider store={ store } >
+        <Router>
+        <SiteNavbar />
           <div>
             <Route exact path='/'component={Landing} />
             <Route path='/ManagerLogin' component={ManagerLogin} />
           </div>
-        </div>
-        
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
