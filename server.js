6062b26803402e12a861fa55cee8cf1780c355e0
Usman <u.ghani2013@gmail.com>
2018-08-05 08:46:13 -0500

    Make react client handle routing in production if server doesn't have matching URI

--- server.js
+++ server.js
@@ -58,6 +58,10 @@ app.use('/api/checks', checks);
 // For prodution only
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // Initialize port and run server
