f1f87319d50f77fd474c3e60c33ceeba157d5d77
Usman <u.ghani2013@gmail.com>
2018-08-05 09:55:21 -0500

    Add comments

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const managers = require('./routes/api/managers'),
 
 const app = express();
 
-
+// Allow CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
