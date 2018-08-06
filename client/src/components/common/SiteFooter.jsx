9f2291d1bb3fd8e6dcfa32b073b52ce401e5315d
Usman <u.ghani2013@gmail.com>
2018-08-06 08:12:49 -0500

    Center text of footer

--- client/src/components/common/SiteFooter.jsx
+++ client/src/components/common/SiteFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => {
   return (
-    <footer>
+    <footer class="text-center ">
       Copyright &copy; {new Date().getFullYear()} ShiftBuddyPro
     </footer>
   )
