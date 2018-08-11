80ace140a63cd3b8deea0e5a6151c31adecd6297
Usman <u.ghani2013@gmail.com>
2018-08-11 08:39:51 -0500

    Add texaco logo example

--- client/src/components/manager/ManagerDashboard.jsx
+++ client/src/components/manager/ManagerDashboard.jsx
@@ -11,7 +11,7 @@ export class ManagerLogin extends Component {
     const { manager } = this.props.managerAuth;
     return (
       <div>
-        <h1 className="text-center">Welcome {manager.name}</h1>
+        <h1 className="text-center">Welcome {manager.name} <img src="//logo.clearbit.com/texaco.com" /></h1> 
         <div>Click <a href="/employeeList">here</a> to view your employees or create a new one.</div>
       </div>
     )
