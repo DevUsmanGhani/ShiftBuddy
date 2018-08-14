23bde1ffe14addb5253f60d55a3c7abefc06c3ac
Usman <u.ghani2013@gmail.com>
2018-08-14 11:45:05 -0500

    Manager dashboard will show first 3 employees

--- client/src/components/manager/Manager.jsx
+++ client/src/components/manager/Manager.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Grid, Row, Col } from 'react-bootstrap';
@@ -5,20 +6,36 @@ import { Grid, Row, Col } from 'react-bootstrap';
 export class Manager extends Component {
   render() {
     const { manager } = this.props.managerAuth;
+    const { employees } = this.props;
+    let i = 0;
     return (
       <Grid className="manager-dashboard">
         <h1>Welcome {manager.name} <img src="//logo.clearbit.com/texaco.com" alt="Company Logo" /></h1> 
         <hr />
         <Row>
-          <Col xs={3}>
-            <div>
-              <h3>Your employees</h3>
+          <Col xsHidden sm={3} lg={2} className="side-box">
+            <div className="employees-box">
+              <h4 className="employees-header">Your Employees</h4>
+              
+              {_.map(employees, employee => {
+                if(i < 3){
+                  i++;
+                  return(
+                    <a key={employee._id + 'dashboard'} className="employees-name" href={`/managers/${manager.id}/employees/${employee._id}`}>{employee.name}</a>
+                  )}
+                })
+              }
+              <a href={`/managers/${manager.id}/employees`} className="view-all">View All</a>
             </div>
-            <div>
-              Recent Shifts: 
+            <div className="shifts-box">
+              <h4 className="shifts-header">Recent Shifts:</h4> 
+              <a className="shifts-name">08/15-B</a>
+              <a className="shifts-name">08/15-A</a>
+              <a className="shifts-name">08/14-C</a>
+              <a className="view-all">View all</a>
             </div>
           </Col>
-          <Col xs={9}>
+          <Col xs={12} sm={9} lg={10}>
             <h2>Activity Log</h2>
             <div className="log-card">
               Steve just made a drop with envelope # 12 for $300.00.
@@ -39,6 +56,7 @@ export class Manager extends Component {
 
 const mapStateToProps = state => ({
   managerAuth: state.managerAuth,
+  employees: state.employees,
 })
 
 
