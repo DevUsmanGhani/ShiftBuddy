e9b67f9451d829d49b879c60f7c84d15f137a954
Usman <u.ghani2013@gmail.com>
2018-08-20 12:38:29 -0500

    Create template for employee profile

--- client/src/components/employees/EmployeeView.jsx
+++ client/src/components/employees/EmployeeView.jsx
@@ -25,30 +25,36 @@ export class EmployeeView extends Component {
 
   render() {
     return (
-      <div>
+      <Grid>
         <Row>
           <Col xs={12}>
             <div className="back-header-container">            
               <div onClick={this.props.history.goBack} className="back"><FontAwesomeIcon icon="chevron-left"/> Back</div>
-              <h1 className="shift-page-header">Shifts - {this.props.employees[this.state.employeeId].name}</h1>
+              <h1 className="shift-page-header">{this.props.employees[this.state.employeeId].name}</h1>
             </div>
           </Col>
         </Row>
         <hr />
-        <Grid>
-          <Row>
-            <Col md={10} mdOffset={1}>
-              {_.map(this.props.shifts, shift => {
+        <Row>
+          <Col xs={12} sm={2}>
+            Arham Ghani <br />
+             :) <br />
+             dob.. <br />
+             last shift: 
+          </Col>
+          <Col xs={12} sm={9}>
+          <h2 className="center">{this.props.employees[this.state.employeeId].name}'s Shifts</h2>
+          {_.map(this.props.shifts, shift => {
                 if (shift.employee === this.state.employeeId)
                 return(<a key={shift._id + 'link'} href={`/shifts/${shift._id}`} ><ShiftsContainer key={shift._id + 'container'}  employee={this.props.employees[shift.employee]} shift={shift} /></a>)
                 }
               )}
-            </Col>
-          </Row>
-        </Grid>
+          </Col>
+        </Row>              
+        
        
 
-      </div>
+      </Grid>
     )   
   }
 }
